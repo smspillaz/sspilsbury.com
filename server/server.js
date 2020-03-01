3a22122597da0fb0c9a9810362280ce1fc5aa2ea
Sam Spilsbury <smspillaz@gmail.com>
2020-03-02 01:21:56 +0200

    server: Use serverApi

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const serverApi = require('./api/api');
 const app = express();
 
 // If you need a backend, e.g. an API, add your custom backend-specific middleware here
-// app.use('/api', myApi);
+app.use('/api', serverApi);
 
 // In production we need to pass these values in instead of relying on webpack
 if (process.env.SERVERLESS !== '1') {
