074c35666987b570719f4840e3454d219a6c6966
Sam Spilsbury <smspillaz@gmail.com>
2020-03-02 01:38:16 +0200

    components: Add TimeoutPager

---
+++ app/components/TimeoutPager/index.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import TrackVisibility from 'react-on-screen';
+
+export class TimeoutPager extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      index: 0,
+    };
+    this.mounted = false;
+    this.timer = 0;
+  }
+
+  componentDidMount() {
+    this.mounted = true;
+    if (this.props.running) {
+      this.timer = window.setInterval(this.changeIndex, this.props.time);
+    }
+  }
+
+  componentDidUpdate(prev) {
+    if (!this.mounted) {
+      return;
+    }
+
+    if (!this.timer) {
+      if (this.props.running) {
+        this.timer = window.setInterval(this.changeIndex, this.props.time);
+      }
+    } else if (!this.props.running) {
+      window.clearInterval(this.timer);
+      this.timer = null;
+    } else if (this.props.time !== prev.time) {
+      window.clearInterval(this.timer);
+      this.timer = window.setInterval(this.changeIndex, this.props.time);
+    }
+  }
+
+  changeIndex = () =>
+    this.setState(state => ({
+      index: (state.index + 1) % this.props.range,
+    }));
+
+  render() {
+    return this.props.render({ index: this.state.index });
+  }
+}
+
+TimeoutPager.propTypes = {
+  time: PropTypes.number.isRequired,
+  range: PropTypes.number.isRequired,
+  render: PropTypes.func.isRequired,
+  running: PropTypes.bool,
+};
+
+TimeoutPager.defaultProps = {
+  running: false,
+};
+
+export const VisibilityTrackingTimeoutPager = props => (
+  <TrackVisibility partialVisibility>
+    {({ isVisible }) => <TimeoutPager running={isVisible} {...props} />}
+  </TrackVisibility>
+);
