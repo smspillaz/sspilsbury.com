954c47149bc7361fa4021f7086f07283c64b95d8
Sam Spilsbury <smspillaz@gmail.com>
2020-03-03 01:10:15 +0200

    containers: Add NotFoundPage

--- app/containers/NotFoundPage/index.js
+++ app/containers/NotFoundPage/index.js
@@ -8,12 +8,15 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 
+import H1 from 'components/H1';
 import messages from './messages';
 
 export default function NotFound() {
   return (
-    <h1>
-      <FormattedMessage {...messages.header} />
-    </h1>
+    <article>
+      <H1>
+        <FormattedMessage {...messages.header} />
+      </H1>
+    </article>
   );
 }
