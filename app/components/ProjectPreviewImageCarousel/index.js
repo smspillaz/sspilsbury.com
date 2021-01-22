ef08483fe1bbcb68aaa15efe939d3a39b4e49c8e
Sam Spilsbury <smspillaz@gmail.com>
2021-01-22 22:54:30 +0200

    ProjectPreviewIamgeCarousel: Add 'visible' key

--- app/components/ProjectPreviewImageCarousel/index.js
+++ app/components/ProjectPreviewImageCarousel/index.js
@@ -12,13 +12,14 @@ export const ProjectPreviewImageCarousel = ({
   <VisibilityTrackingTimeoutPager
     range={images.length}
     time={timeout - (Math.random() * timeout) / 2}
-    render={({ index }) =>
+    render={({ index, visible }) =>
       images.map((imageUrl, i) => (
         <CrossBlurFadeBackgroundImage
           key={imageUrl}
           url={imageUrl}
           selected={i === index}
           styleProps={imageStyleProps}
+          visible={visible}
         />
       ))
     }
