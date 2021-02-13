758db9157e900339cd8e202bdf1e3277b1a0cdf3
Sam Spilsbury <smspillaz@gmail.com>
2021-02-14 01:55:15 +0200

    package/server: Remove ngrok dependency
    Doesn't work on my new macbook :-)

--- server/index.js
+++ server/index.js
@@ -4,12 +4,6 @@ const argv = require('./argv');
 const port = require('./port');
 const logger = require('./logger');
 
-const isDev = process.env.NODE_ENV !== 'production';
-const ngrok =
-  (isDev && process.env.ENABLE_TUNNEL) || argv.tunnel
-    ? require('ngrok')
-    : false;
-
 // get the intended host and port number, use localhost and port 3000 if not provided
 const customHost = argv.host || process.env.HOST;
 const host = customHost || null; // Let http.Server use its default IPv6/4 host
@@ -21,18 +15,7 @@ app.listen(port, host, async err => {
     return logger.error(err.message);
   }
 
-  // Connect to ngrok in dev mode
-  if (ngrok) {
-    let url;
-    try {
-      url = await ngrok.connect(port);
-    } catch (e) {
-      return logger.error(e);
-    }
-    logger.appStarted(port, prettyHost, url);
-  } else {
-    logger.appStarted(port, prettyHost);
-  }
+  logger.appStarted(port, prettyHost);
 
   return null;
 });
